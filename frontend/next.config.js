d1f1a1e7db8e413db9ede1c65d46e06fb21c6bc2
benuhana-oss <benuhana@gmail.com>
2026-03-15 16:58:43 +0200

    Add transpilePackages for Solana wallet adapters

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  transpilePackages: [
+    "@solana/wallet-adapter-react",
+    "@solana/wallet-adapter-wallets"
+  ]
 }
 
 module.exports = nextConfig
